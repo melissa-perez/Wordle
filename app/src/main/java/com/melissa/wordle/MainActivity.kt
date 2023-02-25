eef44cd3234ab62d60deb1f4c1271e4228279348
Melissa Perez <melmperz@gmail.com>
2023-02-24 23:18:20 -0500

    Add 4 letter fun use and some UI elements

--- app/src/main/java/com/melissa/wordle/MainActivity.kt
+++ app/src/main/java/com/melissa/wordle/MainActivity.kt
@@ -2,10 +2,44 @@ package com.melissa.wordle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.melissa.wordle.FourLetterWordList
 
 class MainActivity : AppCompatActivity() {
+    private val NUMBER_OF_GUESSES = 3
+    private var wordToGuess = FourLetterWordList.getRandomFourLetterWord()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //val screenView = findViewById<TextView>(R.id.wordView)
+        //screenView.text = FourLetterWordList.getRandomFourLetterWord()
+    }
+
+    /**
+     * Parameters / Fields:
+     *   wordToGuess : String - the target word the user is trying to guess
+     *   guess : String - what the user entered as their guess
+     *
+     * Returns a String of 'O', '+', and 'X', where:
+     *   'O' represents the right letter in the right place
+     *   '+' represents the right letter in the wrong place
+     *   'X' represents a letter not in the target word
+     */
+    private fun checkGuess(guess: String) : String {
+        var result = ""
+        for (i in 0..3) {
+            if (guess[i] == wordToGuess[i]) {
+                result += "O"
+            }
+            else if (guess[i] in wordToGuess) {
+                result += "+"
+            }
+            else {
+                result += "X"
+            }
+        }
+        return result
     }
 }
