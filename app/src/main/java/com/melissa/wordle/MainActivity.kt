1cd252f069cfb2f2a4a2cf4e48a809fe50839eae
Melissa Perez <melmperz@gmail.com>
2023-02-26 02:38:53 -0500

    Issues showing invisible text fields

--- app/src/main/java/com/melissa/wordle/MainActivity.kt
+++ app/src/main/java/com/melissa/wordle/MainActivity.kt
@@ -12,52 +12,60 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     private var numberOfGuesses: Int = 3
     private var wordToGuess: String = FourLetterWordList.getRandomFourLetterWord()
-    private val correctWordTextView: TextView = findViewById(R.id.correctWordTextView)
-    private val currentGuess: EditText = findViewById(R.id.guessEditText)
-    private val guessBtn: Button = findViewById(R.id.guessEnterButton)
-    private val guess1 : TextView = findViewById(R.id.guessOneText)
-    private val guess1Check: TextView = findViewById(R.id.guessOneCheckText)
-    private val guess2: TextView = findViewById(R.id.guessTwoText)
-    private val guess2Check: TextView = findViewById(R.id.guessTwoCheckText)
-    private val guess3: TextView = findViewById(R.id.guessThreeText)
-    private val guess3Check: TextView = findViewById(R.id.guessThreeCheckText)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // 1. get ids for later use
+        val correctWordTextView: TextView = findViewById(R.id.correctWordTextView)
+        val currentGuess: EditText = findViewById(R.id.guessEditText)
+        val guessBtn: Button = findViewById(R.id.guessEnterButton)
+        val guess1 : TextView = findViewById(R.id.guessOne)
+        //val guess1Check: TextView = findViewById(R.id.guessOneCheck)
+        //val guess2: TextView = findViewById(R.id.guessTwo)
+        //val guess2Check: TextView = findViewById(R.id.guessTwoCheck)
+        //val guess3: TextView = findViewById(R.id.guessThree)
+        //val guess3Check: TextView = findViewById(R.id.guessThreeCheckText)
 
         // 1. get the correct word TV and set it to selected word
         correctWordTextView.text = wordToGuess
         Log.v("word", wordToGuess)
+        Log.v("word", guess1.toString())
 
         // 2. wait for user to guess with guess button click listener
         guessBtn.setOnClickListener{
             val userGuessEntered = currentGuess.text.toString()
             Log.v("guess", userGuessEntered)
-            currentGuess.setText("")
 
             // 1. Check if we have enough guesses to process first
-            if (numberOfGuesses > 0) {
-                numberOfGuesses--
-                Toast.makeText(it.context, "Guesses remaining: $numberOfGuesses", Toast.LENGTH_SHORT).show()
-                if (numberOfGuesses == 2) {
-                    guess1.text = guess1.text.toString().plus(userGuessEntered)
-                    guess1Check.text = guess1Check.text.toString().plus(checkGuess(userGuessEntered))
-                }
-                else if (numberOfGuesses == 1) {
-                    guess2.text = guess2.text.toString().plus(userGuessEntered)
-                    guess2Check.text = guess2Check.text.toString().plus(checkGuess(userGuessEntered))
+            //if (numberOfGuesses > 1) {
+            Log.v("guess", numberOfGuesses.toString())
+
+            Log.v("guess", "Hello from guess 3")
+            Log.v("guess", guess1.text.toString())
+            guess1.visibility = VISIBLE
+               /* else if (numberOfGuesses == 2) {
+                   // guess2.text = guess2.text.toString().plus(userGuessEntered)
+                   // guess2Check.text = guess2Check.text.toString().plus(checkGuess(userGuessEntered))
+                    //guess2.visibility = VISIBLE
+                    //guess2Check.visibility = VISIBLE
                 }
                 else {
-                    guess3.text = guess3.text.toString().plus(userGuessEntered)
-                    guess3Check.text = guess3Check.text.toString().plus(checkGuess(userGuessEntered))
-                }
-            }
-            else {
-                correctWordTextView.visibility = VISIBLE
-                guessBtn.isEnabled = false
-                Toast.makeText(it.context, "Out of guesses!", Toast.LENGTH_SHORT).show()
-            }
+                   // guess3.text = guess3.text.toString().plus(userGuessEntered)
+                    //guess3Check.text = guess3Check.text.toString().plus(checkGuess(userGuessEntered))
+                    //guess3.visibility = VISIBLE
+                    //guess3Check.visibility = VISIBLE
+                }*/
+            numberOfGuesses--
+            Toast.makeText(it.context, "Guesses remaining: $numberOfGuesses", Toast.LENGTH_SHORT).show()
+           // }
+          //  else {
+            correctWordTextView.visibility = VISIBLE
+            guessBtn.isEnabled = false
+            Toast.makeText(it.context, "Out of guesses!", Toast.LENGTH_SHORT).show()
+           // }
+            //guess1.visibility = VISIBLE
+            currentGuess.setText("")
         }
     }
 
